d32ea396102ad6efc66cb256f86911010e78da6b
Maroof Qureshi <123865444+developermaroof@users.noreply.github.com>
2025-04-26 15:47:18 +0500

    Fetched Comments inside a Post

--- services/index.js
+++ services/index.js
@@ -174,3 +174,18 @@ export const submitComment = async (obj) => {
     throw error;
   }
 };
+
+export const getComments = async (slug) => {
+  const query = gql`
+    query GetComments($slug: String!) {
+      comments(where: { post: { slug: $slug } }) {
+        name
+        createdAt
+        comment
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query, { slug });
+  return result.comments;
+};
